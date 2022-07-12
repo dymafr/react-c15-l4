b9c8b0a5bce32a7b16bf119d755e79bd2ddb94c1
dymafr <erwan1309@hotmail.com>
2022-07-12 12:11:15 +0200

    4 files modified

--- src/components/Header/components/HeaderMenu.js
+++ src/components/Header/components/HeaderMenu.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './HeaderMenu.module.scss';
 
-function HeaderMenu() {
+function HeaderMenu({ setPage }) {
   return (
     <ul className={`${styles.MenuContainer} card p-20`}>
+      <li onClick={() => setPage('admin')}>Ajouter une recette</li>
       <li>Wishlist</li>
       <li>Connexion</li>
     </ul>
